79df03f875c3a0303a4e90fe0b70487f31404cfc
vaishnavi rao <vaishnavi rao>
2017-01-30 19:52:18 +0530

    mail

--- src/com/setup/Email.java
+++ src/com/setup/Email.java
@@ -19,8 +19,8 @@ public class Email {
 	@Path("/send")
 	public String send_mail()
 	{
-		final String username = "vaishnavirao17@gmail.com";
-		final String password = "amandambaht*17";
+		final String username = "";
+		final String password = "";
 
 		Properties props = new Properties();
 		props.put("mail.smtp.auth", "true");
@@ -38,9 +38,9 @@ String status;
 		try {
 
 			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress("vaishnavirao17@gmail.com"));
+			message.setFrom(new InternetAddress(""));
 			message.setRecipients(Message.RecipientType.TO,
-				InternetAddress.parse("vaishnavirao17@gmail.com"));
+				InternetAddress.parse(""));
 			message.setSubject("Testing Subject");
 			message.setText("Hello! :)");
 
